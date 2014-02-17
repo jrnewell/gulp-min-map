3008d10d2c1b3fd7abda47125cf0c8f6b64ba466
James Newell <james.newell@gmail.com>
2014-02-17 09:37:40 -0500

    remove console.log debugging messages

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ var _ = require('underscore');
 var path = require('path');
 var url = require('url');
 
+/*
+  optional arguments:
+    autoMin (bool)          - minimize files with a data-min attribute [default=true]
+    defaultMinFile (string) - if autoMin is true, use this min file for all files without a data-min attribute.
+                              if defaultMinFile is null, use the src file name instead [default=null]
+    updateHTML (bool)       - rewrite HTML files to point at the min files [default=true]
+*/
 module.exports = function(type, map, options) {
   opt = options || {};
   opt.autoMin = opt.autoMin || true;
   opt.updateHTML = opt.updateHTML || true;
 
+  // try to determine if this is a remote url or a local reference
   var isLocal = function(srcPath) {
     var srcUrl = url.parse(srcPath, false, true);
     return !(srcUrl.hostname);
   };
 
+  // ignore any elements that already point to a min file
   var isMin = function(srcPath) {
     return (srcPath.indexOf('.min') >= 0);
   };
 
+  // create a resolved pathname relative to the globbing base
   var resolvePath = function(file, srcPath) {
     var normPath = path.normalize(srcPath);
 
-    // are we an "absoulte path"?
+    // are we an "absoulte path"? (e.g. /js/app.js)
     if (normPath.indexOf(path.sep) == 0) {
       return path.join(file.base, normPath);
     }
@@ -34,6 +44,7 @@ module.exports = function(type, map, options) {
     return path.basename(srcPath, extname);
   };
 
+  // string util
   var replaceLastInstance = function(str, text, replaceText) {
     var idx = str.lastIndexOf(text);
     if (idx < 0) return str;
@@ -42,7 +53,8 @@ module.exports = function(type, map, options) {
     return (startStr + replaceText + endStr);
   };
 
-  var resolveMinFile = function(file, srcFile, minFile) {
+  // generate the minFile path using the srcFile and data attribute
+  var genMinFilePath = function(file, srcFile, minFile) {
     // are we an "absoulte path"?
     if (minFile.indexOf(path.sep) == 0) {
       return minFile;
@@ -53,37 +65,35 @@ module.exports = function(type, map, options) {
   };
 
   var addMinFileToMap = function(map, replacedMinFile, elem, file, srcFile, minFile, srcSelector) {
-    var minFile = resolveMinFile(file, srcFile, minFile);
+    var minFile = genMinFilePath(file, srcFile, minFile);
     var normMinFile = resolvePath(file, minFile).replace(file.base, "");
     var fullSrcFile = resolvePath(file, srcFile);
+
+    // update the minFile map/object.
     var srcArray = map[normMinFile] || [];
     if (srcArray.indexOf(fullSrcFile) < 0) {
       srcArray.push(fullSrcFile);
     }
     map[normMinFile] = srcArray;
 
-    console.log("minFile: " + minFile + ", normMinFile: " + normMinFile + ", srcFile: " + srcFile);
-
-    // change dom (either update to .min file, or remove)
+    // change the HTML dom (either update to .min file, or remove depending on
+    // if we have seen this min file before in this HTML file)
     if (opt.updateHTML) {
       if (!replacedMinFile[normMinFile]) {
-        console.log("modifying DOM element");
         elem.removeAttr('data-min');
         elem.attr(srcSelector, path.normalize(minFile));
         replacedMinFile[normMinFile] = true;
       }
       else {
-        console.log("removing DOM element");
         elem.remove();
       }
     }
   };
 
   var minMapFunc = function(map, replacedMinFile, elem, file, srcFile, minFile, srcSelector) {
-    console.log("srcFile: " + srcFile + ", minFile: " + minFile + ", isLocal: " + isLocal(srcFile));
 
+    // if there is no data-min attribute, use a default minFile unless instructed otherwise
     if (srcFile && minFile) {
-      console.log("minMap1: " + srcFile + ", " + minFile);
       addMinFileToMap(map, replacedMinFile, elem, file, srcFile, minFile, srcSelector);
     }
     else if (opt.autoMin && !(elem.attr('data-no-min')) && srcFile && isLocal(srcFile) && !isMin(srcFile)) {
@@ -94,7 +104,6 @@ module.exports = function(type, map, options) {
         var baseName = baseNameNoExt(srcFile);
         minFile = replaceLastInstance(srcFile, baseName, (baseName + ".min"));
       }
-      console.log("minMap2: " + baseName + ", " + srcFile + ", " + minFile);
       addMinFileToMap(map, replacedMinFile, elem, file, srcFile, minFile, srcSelector);
     }
   };
@@ -115,25 +124,23 @@ module.exports = function(type, map, options) {
       });
     };
 
-    var util = require('util');
-    console.log("type = " + type + ", file.cwd: " + file.cwd + ", file.base: " + file.base + ", file.path: " + file.path);
-
     var typeArray;
     if (_.isArray(type)) typeArray = type;
     else if (_.isString(type)) typeArray = [type];
-    else return callback(new Error("invalid type"));
+    else return callback(new Error("Unsupported type paramter type supplied"), undefined);
 
+    var isErr = false;
     typeArray.forEach(function(type) {
-      if (type === 'js') {
-        handleFunc(type, 'script', 'src');
-      }
-      else if (type === 'css') {
-        handleFunc(type, 'link[rel=stylesheet]', 'href');
+      if (type === 'js') handleFunc(type, 'script', 'src');
+      else if (type === 'css') handleFunc(type, 'link[rel=stylesheet]', 'href');
+      else {
+        isErr = true;
+        return callback(new Error("Unsupported type paramter type supplied"), undefined);
       }
     });
+    if (isErr) return;
 
     if (opt.updateHTML) file.contents = new Buffer($.html());
-    console.log('map: ' + util.inspect(map))
     return callback(null, file);
   }
 
